3f6014b37dc2fd85bc0c80a2ccd956bfd2df78aa
fessler <fessler@umich.edu>
2019-12-25 22:13:00 -0500

    Add test

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,5 +6,6 @@ using Test: @test, detect_ambiguities
 @test header_test(:test)
 @test read_rdb_hdr(:test)
 @test h5_get_test(:test)
+@test loadpfile(:test)
 
 @test length(detect_ambiguities(MIRTio)) == 0
