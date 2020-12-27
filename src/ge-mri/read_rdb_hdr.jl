cb665f551589edc107e7f0c6e9c8b29e7dc7f7f0
Jeff Fessler <fessler@umich.edu>
2020-12-27 16:19:08 -0500

    format

--- src/ge-mri/read_rdb_hdr.jl
+++ src/ge-mri/read_rdb_hdr.jl
@@ -22,21 +22,20 @@ Read GE raw (RDB) header for MRI scans
 Returns NamedTuple `ht` with header values accessible by `ht.key`
 """
 function read_rdb_hdr(fid::IOStream)
-	seek(fid, 0)
-	rdbm_rev = read(fid, Float32) # raw header (RDBM) revision number
+    seek(fid, 0)
+    rdbm_rev = read(fid, Float32) # raw header (RDBM) revision number
 
-	if rdbm_rev == Float32(26.002)
-		hd = rdb_hdr_26_002_def()
-		ht = header_read(fid, hd)
-		ht = header_string(ht) # convert Array{Cuchar} to String
-	else
-		throw("unknown RDBM rev $rdbm_rev")
-	end
+    if rdbm_rev == Float32(26.002)
+        hd = rdb_hdr_26_002_def()
+        ht = header_read(fid, hd)
+        ht = header_string(ht) # convert Array{Cuchar} to String
+    else
+        throw("unknown RDBM rev $rdbm_rev")
+    end
 
-	(ht.rdbm_rev != rdbm_rev) &&
-		throw("rev mismatch: $(ht.rdbm_rev) != $rdbm_rev")
+    (ht.rdbm_rev != rdbm_rev) && throw("rev mismatch: $(ht.rdbm_rev) != $rdbm_rev")
 
-	return ht
+    return ht
 end
 
 
@@ -45,5 +44,5 @@ end
 Read from `file`
 """
 function read_rdb_hdr(file::String)
-	return open(read_rdb_hdr, file)
+    return open(read_rdb_hdr, file)
 end
