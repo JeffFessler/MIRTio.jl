2e234834b734ff6d1415ed45134c45af343ada13
Jeff Fessler <JeffFessler@users.noreply.github.com>
2024-05-21 16:22:40 -0400

    Refine type code

--- src/ge-mri/rds.jl
+++ src/ge-mri/rds.jl
@@ -2,7 +2,7 @@ export loadrds
 
 """
     dat = loadrds(fid::IOStream, frame::Int, frsize::Int, ncoil::Int; 
-                  ptsize::Int = 2)
+                  T::Type{<:Union{Int16,Int32}} = Int16)
 
 Load one shot (frame) from MRI data file created with GE's Raw Data Server.
 
@@ -17,12 +17,10 @@ in
   Number of receive coils
 
 option
-- `ptsize::Int`
-  Either 2 (real/complex value stored as Int16; default)
-  or 4 (Int32)
+- `T::Type{<:Union{Int16,Int32}} = Int16` for the base type of (complex) data loaded
   
 out
-- `dat::Array{Complex{Int16/Int32}}` `[frsize, ncoil]`
+- `dat::Array{Complex{T}}` `[frsize, ncoil]`
 
 """
 function loadrds(fid::IOStream, frame::Int, frsize::Int, ncoil::Int; 
@@ -31,7 +29,7 @@ function loadrds(fid::IOStream, frame::Int, frsize::Int, ncoil::Int;
 
     d = Array{Complex{T}}(undef, frsize, ncoil) # one readout
 
-    seek(fid, 2*ptsize*(frame-1)*frsize*ncoil)
+    seek(fid, 2*sizeof(T)*(frame-1)*frsize*ncoil)
 
     read!(fid, vec(d))
 
@@ -45,12 +43,12 @@ end
 Read multiple frames. Here, `frames` is an Int vector/array/range.
 
 out
-- `dat::Array{Complex{Int16/Int32}}` `[frsize, ncoil, length(frames)]`
+- `dat::Array{Complex{T}}` `[frsize, ncoil, length(frames)]`
 """
 function loadrds(fid::IOStream, frames::AbstractArray{<:Int}, frsize::Int, ncoil::Int;
     T::Type{<:Union{Int16,Int32}} = Int16,
 )
-    d = stack([loadrds(fid, frame, frsize, ncoil; sizeof(T)) for frame in frames])
+    d = stack([loadrds(fid, frame, frsize, ncoil; T) for frame in frames])
 
     return d
 end
